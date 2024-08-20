f4c3503776b7c874ee896b6210fd176fcfca0be5
Badris18 <badrivk1008@gmail.com>
2024-08-20 18:14:25 +0530

    route fix

--- routes/api.php
+++ routes/api.php
@@ -3,53 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\api\v2\CategoryController;
+use App\Http\Controllers\api\v2\BrandController;
+use App\Http\Controllers\api\v2\ProductController;
+use App\Http\Controllers\api\v2\CartController;
+use App\Http\Controllers\api\v2\OrderController;
+use App\Http\Controllers\api\v2\OrderItemController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-use App\Http\Controllers\api\v2\AuthController;
-
-Route::post('/register',[AuthController::class, 'register']);
-Route::post('/login',[AuthController::class, 'login']);
-
-Route::middleware(['auth:sanctum'])->group(function(){
 
 
+Route::get('/categories', [CategoryController::class,'index']);
 
-    Route::get('/user', [AuthController::class,'getUser']);
-
-    Route::post('/upload-profile-pic', [AuthController::class, 'upload']);
-
-    Route::post('/update-profile', [AuthController::class, 'updateProfile']);
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-});
-
-use App\Http\Controllers\api\v2\CategoryController;
-use App\Http\Controllers\api\v2\BrandController;
-use App\Http\Controllers\api\v2\SubCategoryController;
-use App\Http\Controllers\api\v2\ProductController;
-
-
-
-Route::get('/categories', [CategoryController::class,'index'])->name('home.index');
-
-Route::get('/brands', [BrandController::class,'index'])->name('home.index');
-
-Route::get('/sub_categories', [SubCategoryController::class,'index']);
+Route::get('/brands', [BrandController::class,'index']);
 
 Route::get('/products', [ProductController::class,'index']);
 
-use App\Http\Controllers\api\v2\CartController;
+Route::get('/carts', [CartController::class,'index']);
 
-Route::middleware(['auth:sanctum'])->group(function(){
+Route::get('/orders', [OrderController::class,'index']);
 
-    Route::get('/cart', [CartController::class,'index']);
-    Route::post('/add-to-cart', [CartController::class, 'addToCart']);
-    Route::post('/remove-from-cart', [CartController::class, 'removeFromCart']);
-    Route::post('/increase-quantity', [CartController::class, 'increaseQuantity']);
-    Route::post('/decrease-quantity', [CartController::class, 'decreaseQuantity']);
-    Route::post('/clear-cart', [CartController::class, 'clearCart']);
-
-});
+Route::get('/order_items', [OrderItemController::class,'index']);
